490d9bc620030552ff609fab13ed7cb68ba636b7
RUDRABOIII <105966888+rudraboiii@users.noreply.github.com>
2022-10-31 17:00:02 +0400

    Add files via upload
    AREA_CALCULATOR v2.1 primary function is  to calculate the area of 2D and even some 3D shapes 
    Before downloading the file make sure there is a Code editor/IDLE to run the program
    
    For setup please follow these steps -
    1 Download and extract the zip file 
    2 In Code editor/IDLE select AREA_CALCULATOR v2.1.py file
    3 run program

---
+++ AREA_CALCULATOR v2.1.py
@@ -0,0 +1,205 @@
+# GREETINGS
+
+print("========================= ")
+print(" AREA CALCULATOR ")
+print("========================= ")
+print ("area_calculator_v2.1 ")
+print("GOOD BARRICADE STUDIOS")
+print ("By_RUDRABOIII")
+print ("  ")
+print ("  ")
+
+
+
+#2D
+
+print (" 2 DIMENSIONAL SHAPES")
+print ("**********************")
+print ("  ")
+print ("  ")
+
+
+print("__________")
+print("RECTANGLE")
+print("__________")
+print("   ")
+print ("  ")
+
+length = int(input("lenght  "))
+width =  int(input("width  "))
+
+area = length * width
+
+print("  ")
+area = print(area)
+print("=================")
+print("  ")
+print("  ")
+
+
+print("_______")
+print("SQUARE")
+print("_______")
+print("   ")
+print ("  ")
+
+s = int(input("side  "))
+Square = s*s
+
+print("   ")
+Square = print(Square)
+print("=================")
+print("   ")
+print("  ")
+
+
+
+print("__________")
+print("TRAPEZIUM")
+print("__________")
+print("   ")
+print ("  ")
+
+
+a = int(input("a  "))
+b = int(input("b  "))
+h = int(input("height  "))
+
+trap = a+b*h/2
+print("  ")
+trap= print(trap)
+print("=================")
+print("   ")
+print("  ")
+
+
+print("__________")
+print("PARALELLOGRAM")
+print("__________")
+print("   ")
+
+h = int(input("height  "))
+b =  int(input("base  "))
+
+parra = h * b
+print(" ")
+parra = print(parra)
+print("=================")
+print("   ")
+print("  ")
+
+
+
+print("_______")
+print("TRIANGLE")
+print("_______")
+print("   ")
+print ("  ")
+
+b = int(input("base  "))
+h = int(input("height  "))
+
+tri = b*h/2
+print("  ")
+tri = print(tri)
+print("=================")
+print("   ")
+print("  ")
+
+
+print("_______")
+print("RHOMBUS")
+print("_______")
+print("   ")
+print ("  ")
+
+d1 = int(input("diagonal 1  "))
+d2 = int(input("diagonal 2  "))
+
+rhom = d1*d2/2
+print("  ")
+rhom = print(rhom)
+print("=================")
+print("   ")
+print("  ")
+
+
+
+print("_______")
+print("CIRCLE")
+print("_______")
+print("   ")
+print ("  ")
+
+
+r = int(input("radius  "))
+circle = r * r * 22/7
+print("  ")
+circle = print(circle)
+print("=================")
+print("   ")
+print("  ")
+
+
+
+print (" 3 DIMENSIONAL SHAPES")
+print ("**********************")
+print ("  ")
+print ("  ")
+
+print("__________")
+print("CUBOID VOLUME")
+print("__________")
+print("   ")
+print ("  ")
+
+l = int(input("lenght  "))
+b =  int(input("width  "))
+h =  int(input("height  "))
+
+aa = l*b*h
+
+print("  ")
+aa = print(aa)
+print("=================")
+print("  ")
+print("  ")
+
+print("__________")
+print("CUBE VOLUME")
+print("__________")
+print("   ")
+print ("  ")
+
+s = int(input("side "))
+
+vol = s*s*s
+
+print("  ")
+vol = print(vol)
+print("=================")
+print("  ")
+print("  ")
+
+print("_______")
+print("CYLINDER")
+print("_______")
+print("   ")
+print ("  ")
+
+r = int(input("radius  "))
+h = int(input("height  "))
+
+cylinder = r*r*22/7*h
+print("  ")
+cylinder = print(cylinder)
+print("=================")
+print("   ")
+print("  ")
+
+print ("  ")
+print("   ")
+print(" ======================== ")
+print(" END ")
+print(" ======================== ")
+   
